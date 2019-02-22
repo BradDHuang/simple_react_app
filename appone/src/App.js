857e57e9a69f2fe37f9ef0002d7584fe80e8c1db
BradHuang <adghbc01@tamu.edu>
2019-02-22 01:06:06 +0000

    react app Menu.

--- appone/src/App.js
+++ appone/src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import CateClicked from "./CateClicked";
 
 class App extends Component {
+  
+  state = { cateList: [], err: null, cateItems: null, title: "" };
+  
+  componentDidMount() {
+    axios.get("https://stream-restaurant-menu-svc.herokuapp.com/category")
+      .then(res => {
+        // console.log(res);
+        this.setState({ cateList: res.data });
+      })
+      .catch(err => this.setState({ err }));
+  }
+  
+  onCateClick = (cate_short_name) => {
+    axios.get(`https://stream-restaurant-menu-svc.herokuapp.com/item?category=${cate_short_name}`)
+      .then(res => {
+        // console.log(res);
+        this.setState({ title: cate_short_name });
+        this.setState({ cateItems: res.data });
+      })
+      .catch(err => this.setState({ err }));
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h4>Menu Categories</h4>
+        <div className="cate_list">
+        <ul>
+          {this.state.cateList.map(({name, short_name}) => {
+            return (
+              <li className="cate_row" key={name} onClick={() => this.onCateClick(short_name)}>
+                {`${name} - (${short_name})`}
+              </li>
+            );
+          })}
+        </ul>
+        </div>
+        
+        <CateClicked items={this.state.cateItems} cateName={this.state.title}/>
       </div>
     );
   }
 }
 
 export default App;
+
---
+++ appone/src/CateClicked.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class CateClicked extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    
+    render() {
+        // console.log(this.props.items)
+        return(
+            <div className="items_list">
+                {this.props.items ? (
+                <div>
+                    <h4>{`Items in Category: (${this.props.cateName})`}</h4>
+                    <table>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.props.items.map(item => {
+                        return(<tr key={item.name}>
+                            <td>{item.name}</td>
+                            <td>{item.description}</td>
+                        </tr>);
+                    })}
+                    </tbody>
+                    </table>
+                    </div>
+                ) : null}
+                
+            </div>
+        );
+    }
+}
+
+export default CateClicked;
